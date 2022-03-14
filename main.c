10cb651b836f238fd7cdf7b12c49ba76eef84bd6
xkralo05 <62209993+xkralo05@users.noreply.github.com>
2022-03-14 17:07:08 +0100

    Delete main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "Scanner.h"
-#include "parser.h"
-
-int main(){
-    int parsingResult;
-    parsingResult = parse();
-    fprintf(stderr, "Parsing output: %d\n", parsingResult);
-    return parsingResult;
-}
